fbf706350743b0ce380e8d1672ccd948266d5b1a
zhangbo <363230482@qq.com>
2018-09-30 17:58:16 +0800

    netty ObjectEncoder/ObjectDecoder

---
+++ Algorithms/src/main/java/com/netty/qwzn/chapter7/PojoClient.java
@@ -0,0 +1,73 @@
+package com.netty.qwzn.chapter7;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.serialization.ClassResolvers;
+import io.netty.handler.codec.serialization.ObjectDecoder;
+import io.netty.handler.codec.serialization.ObjectEncoder;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author zhangbo
+ */
+public class PojoClient {
+
+    public static void main(String[] args) throws InterruptedException {
+        NioEventLoopGroup group = new NioEventLoopGroup();
+
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group)
+                    .channel(NioSocketChannel.class)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline()
+                                    .addLast(new ObjectDecoder(ClassResolvers.weakCachingResolver(this.getClass().getClassLoader())))
+                                    .addLast(new ObjectEncoder())
+                                    .addLast(new ClientHandler());
+                        }
+                    });
+
+            ChannelFuture future = bootstrap.connect("127.0.0.1", 9090).sync();
+            future.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+
+
+    @Slf4j
+    static class ClientHandler extends ChannelInboundHandlerAdapter {
+
+        @Override
+        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+            ctx.writeAndFlush(new User(1, "netty client"));
+        }
+
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            User user = (User) msg;
+            log.info("---------read response from server: {}", user);
+        }
+
+        @Override
+        public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+            ctx.flush();
+            ctx.close().sync();
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+            log.error("client exception", cause);
+            ctx.channel().closeFuture().sync();
+        }
+    }
+    
+}
