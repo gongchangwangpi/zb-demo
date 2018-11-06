f7641d96ae96b5f094b4b8178ab95b24daab3360
zhangbo <363230482@qq.com>
2018-11-06 11:34:33 +0800

    netty http server

---
+++ Algorithms/src/main/java/com/netty/qwzn/chpater10/HttpFileServer.java
@@ -0,0 +1,86 @@
+package com.netty.qwzn.chpater10;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpVersion;
+import io.netty.handler.stream.ChunkedWriteHandler;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author zhangbo
+ */
+public class HttpFileServer {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        String url = "";
+
+        EventLoopGroup boss = new NioEventLoopGroup(1);
+        EventLoopGroup worker = new NioEventLoopGroup();
+        try {
+
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(boss, worker).channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast("http-decoder", new HttpRequestDecoder())
+                                    .addLast("http-aggregator", new HttpObjectAggregator(65536))
+                                    .addLast("http-encoder", new HttpResponseEncoder())
+                                    .addLast("http-chunked", new ChunkedWriteHandler())
+                                    .addLast("http-file-server", new HttpFileServerHandler(url));
+                        }
+                    });
+
+            ChannelFuture channelFuture = serverBootstrap.bind(8080).sync();
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            boss.shutdownGracefully();
+            worker.shutdownGracefully();
+        }
+    }
+    
+    @Slf4j
+    static class HttpFileServerHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
+        private String url;
+        HttpFileServerHandler(String url) {
+            this.url = url;
+        }
+
+        @Override
+        protected void channelRead0(ChannelHandlerContext ctx, FullHttpRequest msg) throws Exception {
+            log.info("http file server: read --- {}", msg.toString());
+            
+            ByteBuf byteBuf = Unpooled.copiedBuffer("hello http".getBytes());
+            
+            ctx.channel().write(new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, byteBuf));
+        }
+        
+        @Override
+        public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+            ctx.flush();
+            ctx.close().sync();
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+            log.error("server exception", cause);
+            ctx.channel().closeFuture().sync();
+        }
+    }
+}
