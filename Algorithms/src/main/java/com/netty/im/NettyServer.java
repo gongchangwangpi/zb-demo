64836f01c305ff23a490f58970119dd363ac17ea
zhangbo <363230482@qq.com>
2018-11-12 17:49:09 +0800

    netty im

---
+++ Algorithms/src/main/java/com/netty/im/NettyServer.java
@@ -0,0 +1,63 @@
+package com.netty.im;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author zhangbo
+ */
+@Slf4j
+public class NettyServer {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        NioEventLoopGroup boss = new NioEventLoopGroup(1);
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        bootstrap.group(boss, worker).channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new StringDecoder())
+                                .addLast(new SimpleChannelInboundHandler<String>() {
+                                    @Override
+                                    protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+                                        log.info("netty server receive msg : {}", msg);
+                                    }
+                                });
+                    }
+                });
+
+        ChannelFuture channelFuture = bind(bootstrap, 3306);
+
+        channelFuture.channel().closeFuture().sync();
+
+    }
+
+    private static ChannelFuture bind(ServerBootstrap bootstrap, int port) throws InterruptedException {
+        return bootstrap.bind(1000).sync().addListener(new GenericFutureListener<Future<? super Void>>() {
+                @Override
+                public void operationComplete(Future<? super Void> future) throws Exception {
+                    if (future.isSuccess()) {
+                        log.info("netty server run success, bind port: {}", port);
+                    } else {
+                        // 绑定端口失败，自动重写绑定
+                        log.info("netty server run fail, auto rebind");
+                        bind(bootstrap, port + 1);
+                    }
+                }
+            });
+    }
+
+}
