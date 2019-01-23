86fff4ebdc0caad7dc78bb8eb23b297f4f43d830
zhangbo <363230482@qq.com>
2019-01-23 16:40:32 +0800

    test

---
+++ Algorithms/src/main/java/com/dubbo/test/serialize/Parent.java
@@ -0,0 +1,23 @@
+package com.dubbo.test.serialize;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * @author zhangbo
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Parent {
+    
+    private Long id;
+    
+    private String name;
+    
+    private String code;
+    
+}
---
+++ Algorithms/src/main/java/com/dubbo/test/serialize/Son.java
@@ -0,0 +1,19 @@
+package com.dubbo.test.serialize;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * @author zhangbo
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Son extends Parent {
+    
+    private String code;
+
+}
