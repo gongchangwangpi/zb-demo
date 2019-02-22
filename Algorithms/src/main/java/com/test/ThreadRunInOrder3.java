3b75b2e3615cdee5e3fe8dd8a38f82f8a49d8692
zhangbo <363230482@qq.com>
2019-02-22 15:12:20 +0800

    test

---
+++ Algorithms/src/main/java/com/test/ThreadRunInOrder3.java
@@ -0,0 +1,46 @@
+package com.test;
+
+import com.books.bingfayishu.d4.SleepUtils;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 3个线程，按顺序执行
+ * 
+ * @author zhangbo
+ */
+@Slf4j
+public class ThreadRunInOrder3 {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        MyThread t1 = new MyThread("t1");
+        MyThread t2 = new MyThread("t2");
+        MyThread t3 = new MyThread("t3");
+
+        // 最终是一个线程来执行，通过LinkedBlockingQueue来排队
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        
+        executorService.execute(t1);
+        executorService.execute(t2);
+        executorService.execute(t3);
+        
+        executorService.shutdown();
+    }
+    
+    static class MyThread extends Thread {
+        String name;
+        public MyThread(String name) {
+            super(name);
+            this.name = name;
+        }
+        @Override
+        public void run() {
+            log.info("------- run {}", name);
+            SleepUtils.second(1);
+        }
+    }
+    
+}
---
+++ Algorithms/src/main/java/com/test/ThreadRunInOrder4.java
@@ -0,0 +1,56 @@
+package com.test;
+
+import com.books.bingfayishu.d4.SleepUtils;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 3个线程，按顺序执行
+ * join
+ * 
+ * @author zhangbo
+ */
+@Slf4j
+public class ThreadRunInOrder4 {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread main = Thread.currentThread();
+
+        MyThread t1 = new MyThread("t1");
+        MyThread t2 = new MyThread("t2");
+        MyThread t3 = new MyThread("t3");
+
+        t1.start();
+        // join的实现
+        synchronized (t1) {
+            while (t1.isAlive()) {
+                t1.wait();
+            }
+        }
+        t2.start();
+        synchronized (t2) {
+            while (t2.isAlive()) {
+                t2.wait();
+            }
+        }
+        t3.start();
+        synchronized (t3) {
+            while (t3.isAlive()) {
+                t3.wait();
+            }
+        }
+        
+        log.info("------main ");
+    }
+    
+    static class MyThread extends Thread {
+        public MyThread(String name) {
+            super(name);
+        }
+        @Override
+        public void run() {
+            log.info("------- run ");
+            SleepUtils.second(1);
+        }
+    }
+    
+}
