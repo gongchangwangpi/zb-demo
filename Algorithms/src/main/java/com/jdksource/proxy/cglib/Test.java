9c577cf960cea86a5fb3eee17bd840a7d33b9b81
zhangbo <363230482@qq.com>
2019-05-10 11:11:44 +0800

    动态代理

--- Algorithms/src/main/java/com/jdksource/proxy/cglib/Test.java
+++ Algorithms/src/main/java/com/jdksource/proxy/cglib/Test.java
@@ -23,11 +23,128 @@ public class Test {
         enhancer.setCallback(new LogInterceptor());
         // 使用织入器创建子类
         HelloService helloService = (HelloService) enhancer.create();
-
+        // 实际调用的是子类的hello()方法，里面调用了LogInterceptor.intercept()方法
+        // methodProxy.invokeSuper(target, args)最终调用生成类的CGLIB$hello$0(){super.hello();}
         String res = helloService.hello("test");
 
         System.out.println(res);
 
     }
     
+    
+    // 生成的子类核心逻辑如下：
+    
+    //    final String CGLIB$hello$0(String var1) {
+    //        return super.hello(var1);
+    //    }
+    //
+    //    public final String hello(String var1) {
+    //      var10000 为自己编写的LogInterceptor
+    //        MethodInterceptor var10000 = this.CGLIB$CALLBACK_0;
+    //        if (var10000 == null) {
+    //            CGLIB$BIND_CALLBACKS(this);
+    //            var10000 = this.CGLIB$CALLBACK_0;
+    //        }
+    //
+    //        return var10000 != null ? (String)var10000.intercept(this, CGLIB$hello$0$Method, new Object[]{var1}, CGLIB$hello$0$Proxy) : super.hello(var1);
+    //    }
+    
+    // 
+
+
+    // ----------------------------------------
+    // MethodProxy.invokeSuper
+    // public Object invokeSuper(Object obj, Object[] args) throws Throwable {
+    //        try {
+    //            init();
+    //            FastClassInfo fci = fastClassInfo;
+    //      fci.f2 也是动态生成的，如下
+    //            return fci.f2.invoke(fci.i2, obj, args);
+    //        } catch (InvocationTargetException e) {
+    //            throw e.getTargetException();
+    //        }
+    //    }
+    
+    // ----------------------------------------
+    // public class HelloServiceImpl$$EnhancerByCGLIB$$4cf0a51$$FastClassByCGLIB$$28ee97dd extends FastClass
+    
+    // public Object invoke(int var1, Object var2, Object[] var3) throws InvocationTargetException {
+    //        4cf0a51 var10000 = (4cf0a51)var2;
+    //        int var10001 = var1;
+    //
+    //        try {
+    //            switch(var10001) {
+    //            case 0:
+    //                return new Boolean(var10000.equals(var3[0]));
+    //            case 1:
+    //                return var10000.toString();
+    //            case 2:
+    //                return new Integer(var10000.hashCode());
+    //            case 3:
+    //                return var10000.newInstance((Class[])var3[0], (Object[])var3[1], (Callback[])var3[2]);
+    //            case 4:
+    //                return var10000.newInstance((Callback)var3[0]);
+    //            case 5:
+    //                return var10000.newInstance((Callback[])var3[0]);
+    //            case 6:
+    //                var10000.setCallback(((Number)var3[0]).intValue(), (Callback)var3[1]);
+    //                return null;
+    //            case 7:
+    //                return var10000.hello((String)var3[0]);
+    //            case 8:
+    //                4cf0a51.CGLIB$SET_STATIC_CALLBACKS((Callback[])var3[0]);
+    //                return null;
+    //            case 9:
+    //                4cf0a51.CGLIB$SET_THREAD_CALLBACKS((Callback[])var3[0]);
+    //                return null;
+    //            case 10:
+    //                var10000.setCallbacks((Callback[])var3[0]);
+    //                return null;
+    //            case 11:
+    //                return var10000.getCallback(((Number)var3[0]).intValue());
+    //            case 12:
+    //                return var10000.getCallbacks();
+    //            case 13:
+    //                return 4cf0a51.CGLIB$findMethodProxy((Signature)var3[0]);
+    //            case 14:
+    //                4cf0a51.CGLIB$STATICHOOK1();
+    //                return null;
+    //            case 15:
+    //                return var10000.CGLIB$clone$5();
+    //            case 16:
+    //          此处调用代理子类的CGLIB$hello$0方法，在里面调用父类的hello() // super.hello()
+    //                return var10000.CGLIB$hello$0((String)var3[0]);
+    //            case 17:
+    //                return new Boolean(var10000.CGLIB$equals$2(var3[0]));
+    //            case 18:
+    //                return new Integer(var10000.CGLIB$hashCode$4());
+    //            case 19:
+    //                return var10000.CGLIB$toString$3();
+    //            case 20:
+    //                var10000.CGLIB$finalize$1();
+    //                return null;
+    //            case 21:
+    //                var10000.wait();
+    //                return null;
+    //            case 22:
+    //                var10000.wait(((Number)var3[0]).longValue(), ((Number)var3[1]).intValue());
+    //                return null;
+    //            case 23:
+    //                var10000.wait(((Number)var3[0]).longValue());
+    //                return null;
+    //            case 24:
+    //                return var10000.getClass();
+    //            case 25:
+    //                var10000.notify();
+    //                return null;
+    //            case 26:
+    //                var10000.notifyAll();
+    //                return null;
+    //            }
+    //        } catch (Throwable var4) {
+    //            throw new InvocationTargetException(var4);
+    //        }
+    //
+    //        throw new IllegalArgumentException("Cannot find matching method/constructor");
+    //    }
 }
--- Algorithms/src/main/java/com/jdksource/proxy/jdk/Test.java
+++ Algorithms/src/main/java/com/jdksource/proxy/jdk/Test.java
@@ -38,4 +38,103 @@ public class Test {
 
     }
     
+    // 生成代理类如下： 
+    
+    //package com.sun.proxy;
+    //
+    //import com.jdksource.proxy.jdk.IHelloService;
+    //import com.jdksource.proxy.jdk.IHelloService2;
+    //import java.lang.reflect.InvocationHandler;
+    //import java.lang.reflect.Method;
+    //import java.lang.reflect.Proxy;
+    //import java.lang.reflect.UndeclaredThrowableException;
+    //
+    //public final class $Proxy0 extends Proxy implements IHelloService, IHelloService2 {
+    //    private static Method m1;
+    //    private static Method m5;
+    //    private static Method m2;
+    //    private static Method m3;
+    //    private static Method m4;
+    //    private static Method m0;
+    //
+    //    public $Proxy0(InvocationHandler var1) throws  {
+    //        super(var1);
+    //    }
+    //
+    //    public final boolean equals(Object var1) throws  {
+    //        try {
+    //            return (Boolean)super.h.invoke(this, m1, new Object[]{var1});
+    //        } catch (RuntimeException | Error var3) {
+    //            throw var3;
+    //        } catch (Throwable var4) {
+    //            throw new UndeclaredThrowableException(var4);
+    //        }
+    //    }
+    //
+    //    public final String hello2(String var1) throws  {
+    //        try {
+    //          此处调用自定义的LogInvocationHandler的invoke方法，里面使用反射调用目标方法
+    //            return (String)super.h.invoke(this, m5, new Object[]{var1});
+    //        } catch (RuntimeException | Error var3) {
+    //            throw var3;
+    //        } catch (Throwable var4) {
+    //            throw new UndeclaredThrowableException(var4);
+    //        }
+    //    }
+    //
+    //    public final String toString() throws  {
+    //        try {
+    //            return (String)super.h.invoke(this, m2, (Object[])null);
+    //        } catch (RuntimeException | Error var2) {
+    //            throw var2;
+    //        } catch (Throwable var3) {
+    //            throw new UndeclaredThrowableException(var3);
+    //        }
+    //    }
+    //
+    //    public final String hello(String var1) throws  {
+    //        try {
+    //            return (String)super.h.invoke(this, m3, new Object[]{var1});
+    //        } catch (RuntimeException | Error var3) {
+    //            throw var3;
+    //        } catch (Throwable var4) {
+    //            throw new UndeclaredThrowableException(var4);
+    //        }
+    //    }
+    //
+    //    public final void defaultSayHi() throws  {
+    //        try {
+    //            super.h.invoke(this, m4, (Object[])null);
+    //        } catch (RuntimeException | Error var2) {
+    //            throw var2;
+    //        } catch (Throwable var3) {
+    //            throw new UndeclaredThrowableException(var3);
+    //        }
+    //    }
+    //
+    //    public final int hashCode() throws  {
+    //        try {
+    //            return (Integer)super.h.invoke(this, m0, (Object[])null);
+    //        } catch (RuntimeException | Error var2) {
+    //            throw var2;
+    //        } catch (Throwable var3) {
+    //            throw new UndeclaredThrowableException(var3);
+    //        }
+    //    }
+    //
+    //    static {
+    //        try {
+    //            m1 = Class.forName("java.lang.Object").getMethod("equals", Class.forName("java.lang.Object"));
+    //            m5 = Class.forName("com.jdksource.proxy.jdk.IHelloService2").getMethod("hello2", Class.forName("java.lang.String"));
+    //            m2 = Class.forName("java.lang.Object").getMethod("toString");
+    //            m3 = Class.forName("com.jdksource.proxy.jdk.IHelloService").getMethod("hello", Class.forName("java.lang.String"));
+    //            m4 = Class.forName("com.jdksource.proxy.jdk.IHelloService").getMethod("defaultSayHi");
+    //            m0 = Class.forName("java.lang.Object").getMethod("hashCode");
+    //        } catch (NoSuchMethodException var2) {
+    //            throw new NoSuchMethodError(var2.getMessage());
+    //        } catch (ClassNotFoundException var3) {
+    //            throw new NoClassDefFoundError(var3.getMessage());
+    //        }
+    //    }
+    //}
 }
