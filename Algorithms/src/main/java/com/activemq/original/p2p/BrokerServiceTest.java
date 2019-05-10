9a3e3f1bb45caf4500d664bd783802d117ba0b29
zhangbo <363230482@qq.com>
2019-05-10 15:54:18 +0800

    avtivemq

--- Algorithms/src/main/java/com/activemq/original/p2p/BrokerServiceTest.java
+++ Algorithms/src/main/java/com/activemq/original/p2p/BrokerServiceTest.java
@@ -3,6 +3,10 @@ package com.activemq.original.p2p;
 import org.apache.activemq.broker.BrokerService;
 
 /**
+ * 自己启动一个本地的activemq服务，会在当前用户目录生成
+ * activemq-data/localhost/KahaDB的储存文件
+ * 
+ * 
  * @author zhangbo
  */
 public class BrokerServiceTest {
