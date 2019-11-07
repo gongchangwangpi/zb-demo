6c851e1e5fd053533efe03f45ca4d22996aa9668
zhangbo <bo.zhang@kankanai.com.cn>
2019-11-07 15:19:23 +0800

    reduce test

---
+++ Algorithms/src/main/java/com/jdksource/java8/ReduceTest.java
@@ -0,0 +1,30 @@
+package com.jdksource.java8;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+/**
+ * @author zhangbo
+ * @date 2019-11-07
+ */
+public class ReduceTest {
+
+    public static void main(String[] args) {
+
+        Integer integer = Stream.of(1, 2, 3, 4, 5).reduce(0, (i, j) -> {
+            return i + j;
+        });
+        System.out.println(integer);
+
+        Integer i1 = Stream.of(1, 2, 3, 4, 5).reduce(1, (i, j) -> {
+            return i * j;
+        });
+        System.out.println(i1);
+
+        Optional<Integer> optional = Stream.of(1, 2, 3, 4, 5).reduce((i, j) -> {
+            return i * j;
+        });
+        System.out.println(optional.orElse(-1));
+    }
+
+}
