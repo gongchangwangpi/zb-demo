856256447aa379a46e6f5b9eea4474d345a93843
zhangbo <bo.zhang@kankanai.com.cn>
2020-03-01 16:50:22 +0800

    kafka

--- Algorithms/src/main/java/com/kafka/d1/ConsumerTest.java
+++ Algorithms/src/main/java/com/kafka/d1/ConsumerTest.java
@@ -1,5 +1,6 @@
 package com.kafka.d1;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -7,6 +8,7 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.Properties;
+
 
 /**
  * @author zhangbo
@@ -19,8 +21,8 @@ public class ConsumerTest {
         Properties props = new Properties();
         props.setProperty("bootstrap.servers", "localhost:9093,localhost:9092,localhost:9091");
 //        props.setProperty("zookeeper", "localhost:2181");
-        props.setProperty("group.id", "test");
-        props.setProperty("enable.auto.commit", "true");
+        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "test");
+        props.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
         // 延迟提交offset
         props.setProperty("auto.commit.interval.ms", "1000");
         props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
@@ -35,6 +37,13 @@ public class ConsumerTest {
             for (ConsumerRecord<String, String> record : records) {
                 System.out.printf("partition = %d, offset = %d, key = %s, value = %s%n", record.partition(), record.offset(), record.key(), record.value());
             }
+            // 需设置自动提交offset为false
+            // 同步阻塞提交
+//            consumer.commitSync();
+//            // 异步提交
+//            consumer.commitAsync((metadataMap, exception) -> {
+//
+//            });
         }
 
     }
