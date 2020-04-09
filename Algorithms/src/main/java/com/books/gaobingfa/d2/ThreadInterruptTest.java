7db6a930956d4ad3f774d9e6edb969be8a59e2ea
zhangbo <363230482@qq.com>
2020-04-09 19:24:08 +0800

    interrupt thread

--- Algorithms/src/main/java/com/books/gaobingfa/d2/ThreadInterruptTest.java
+++ Algorithms/src/main/java/com/books/gaobingfa/d2/ThreadInterruptTest.java
@@ -1,30 +1,32 @@
 package com.books.gaobingfa.d2;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by books on 2017/4/17.
- */
+ * */
+@Slf4j
 public class ThreadInterruptTest {
 
     public static void main(String[] args) throws InterruptedException {
         Thread t1 = new Thread() {
             @Override
             public void run() {
-                while (true) {
-                    if (Thread.currentThread().isInterrupted()) {
-                        System.out.println("Interruted!");
-                        break;
-                    }
-                    try {
-                        Thread.sleep(2000);
-                    } catch (InterruptedException e) {
-                        System.out.println("Interruted When Sleep");
-                        //设置中断状态
-                        Thread.currentThread().interrupt();
-                    }
-                    Thread.yield();
+                try {
+                    TimeUnit.SECONDS.sleep(10);
+                } catch (InterruptedException e) {
+                    //
+                    log.error("--- InterruptedException");
+                    // 线程阻塞在sleep等方法上，仅会收到InterruptedException异常，不会设置interrupt标记：false
+                    log.error("isInterrupted = {}", Thread.currentThread().isInterrupted());
                 }
             }
         };
 
+        t1.start();
+        TimeUnit.MILLISECONDS.sleep(10);
+        t1.interrupt();
     }
 }
