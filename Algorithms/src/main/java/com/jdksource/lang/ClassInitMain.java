b7da9eebb7266b9d7196c373462500264d5c261d
张玻 <zhangbo@mydataway.com>
2020-05-11 16:21:42 +0800

    load class init test

--- Algorithms/src/main/java/com/jdksource/lang/ClassInitMain.java
+++ Algorithms/src/main/java/com/jdksource/lang/ClassInitMain.java
@@ -7,11 +7,15 @@ public class ClassInitMain {
 
     public static void main(String[] args) throws Exception {
 
+        // 会执行类的初始化操作
 //        Class<?> clz = Class.forName("com.jdksource.lang.ClassInitTest");
         Class<?> clz = Class.forName("com.jdksource.lang.ClassInitTest", true, ClassInitMain.class.getClassLoader());
+
         // 不初始化class，不会执行static代码块等
 //        Class<?> clz = Class.forName("com.jdksource.lang.ClassInitTest", false, ClassInitMain.class.getClassLoader());
-        
+        // 不会执行初始化操作
+//        Class<ClassInitTest> clz = ClassInitTest.class;
+
         System.out.println(clz);
 
 //        clz.newInstance();
