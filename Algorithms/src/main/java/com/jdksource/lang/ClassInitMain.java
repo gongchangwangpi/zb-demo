1fe281382535854b9f1167011c3afc1e19baff05
张玻 <zhangbo@mydataway.com>
2020-05-12 09:32:00 +0800

    load class init test

--- Algorithms/src/main/java/com/jdksource/lang/ClassInitMain.java
+++ Algorithms/src/main/java/com/jdksource/lang/ClassInitMain.java
@@ -1,5 +1,8 @@
 package com.jdksource.lang;
 
+import java.net.URL;
+import java.net.URLClassLoader;
+
 /**
  * @author zhangbo
  */
@@ -9,16 +12,22 @@ public class ClassInitMain {
 
         // 会执行类的初始化操作
 //        Class<?> clz = Class.forName("com.jdksource.lang.ClassInitTest");
-        Class<?> clz = Class.forName("com.jdksource.lang.ClassInitTest", true, ClassInitMain.class.getClassLoader());
+//        Class<?> clz = Class.forName("com.jdksource.lang.ClassInitTest", true, ClassInitMain.class.getClassLoader());
 
         // 不初始化class，不会执行static代码块等
 //        Class<?> clz = Class.forName("com.jdksource.lang.ClassInitTest", false, ClassInitMain.class.getClassLoader());
-        // 不会执行初始化操作
+
+        // 不会执行类初始化操作
 //        Class<ClassInitTest> clz = ClassInitTest.class;
+
+        // 不会执行类初始化操作
+        URLClassLoader classLoader = new URLClassLoader(new URL[]{new URL("file:\\D:\\IdeaProjects\\github\\zb-demo\\Algorithms\\target\\classes")});
+        Class<?> clz = classLoader.loadClass("com.jdksource.lang.ClassInitTest");
+
 
         System.out.println(clz);
 
-//        clz.newInstance();
+        clz.newInstance();
 
     }
     
