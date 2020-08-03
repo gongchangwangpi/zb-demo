a67d23b4cbc313334d953489b67d2964c941b03e
张玻 <zhangbo@mydataway.com>
2020-08-03 11:48:42 +0800

    feat(easyexcel test):

--- Algorithms/src/main/java/com/test/easyexcel/BrokeragePolicy.java
+++ Algorithms/src/main/java/com/test/easyexcel/BrokeragePolicy.java
@@ -7,8 +7,8 @@
  */
 package com.test.easyexcel;
 
-import com.alibaba.excel.annotation.ExcelColumnNum;
 import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.annotation.format.DateTimeFormat;
 import com.alibaba.excel.metadata.BaseRowModel;
 import lombok.Getter;
 import lombok.Setter;
@@ -29,135 +29,135 @@ public class BrokeragePolicy extends BaseRowModel implements Serializable {
     /**
      * 出单机构名称
      */
-    @ExcelColumnNum(value = 0)
+    @ExcelProperty(index = 0)
     private String enterpriseName;
     /**
      * 承保公司名称defalt
      */
-    @ExcelColumnNum(value = 1)
+    @ExcelProperty(index = 1)
     private String insuranceCompanyName;
     /**
      * 渠道
      */
-    @ExcelColumnNum(value = 2)
+    @ExcelProperty(index = 2)
     private String channel;
     /**
      * 险种
      */
-    @ExcelColumnNum(value = 3)
+    @ExcelProperty(index = 3)
     private String coverage;
     /**
      * 保单号
      */
-    @ExcelColumnNum(value = 4)
+    @ExcelProperty(index = 4)
     private String policyNo;
     /**
      * 批单号
      */
-    @ExcelColumnNum(value = 5)
+    @ExcelProperty(index = 5)
     private String endorseNo;
     /**
      * 车牌号
      */
-    @ExcelColumnNum(value = 6)
+    @ExcelProperty(index = 6)
     private String licensePlate;
     /**
      * 投保人
      */
-    @ExcelColumnNum(value = 7)
+    @ExcelProperty(index = 7)
     private String policyHolder;
     /**
      * 被保险人
      */
-    @ExcelColumnNum(value = 8)
+    @ExcelProperty(index = 8)
     private String policyInsured;
     /**
      * 保险起期
      */
-    @ExcelColumnNum(value = 9)
-    @ExcelProperty(format = "yyyy/MM/dd")
+    @ExcelProperty(index = 9)
+    @DateTimeFormat(value = "yyyy/MM/dd")
     private Date beginDate;
     /**
      * 保险止期
      */
-    @ExcelColumnNum(value = 10)
-    @ExcelProperty(format = "yyyy/MM/dd")
+    @ExcelProperty(index = 10)
+    @DateTimeFormat(value = "yyyy/MM/dd")
     private Date endDate;
     /**
      * 出单日期
      */
-    @ExcelColumnNum(value = 11)
-    @ExcelProperty(format = "yyyy/MM/dd")
+    @ExcelProperty(index = 11)
+    @DateTimeFormat(value = "yyyy/MM/dd")
     private Date issueDate;
     /**
      * 保费-保费
      */
-    @ExcelColumnNum(value = 12)
+    @ExcelProperty(index = 12)
     private BigDecimal premiumAmount;
     /**
      * 保费-车船税
      */
-    @ExcelColumnNum(value = 13)
+    @ExcelProperty(index = 13)
     private BigDecimal vehicleVesselTax;
     /**
      * 保费-保费合计=保费+车船税
      */
-    @ExcelColumnNum(value = 14)
+    @ExcelProperty(index = 14)
     private BigDecimal totalPremiumAmount;
     /**
      * 应收保险公司手续费--手续费比例
      */
-    @ExcelColumnNum(value = 15)
+    @ExcelProperty(index = 15)
     private String procedureFeeRate;
     /**
      * 应收保险公司手续费--手续费金额
      */
-    @ExcelColumnNum(value = 16)
+    @ExcelProperty(index = 16)
     private BigDecimal procedureFeeAmount;
     /**
      * 手续费(票外）签约主体
      */
-    @ExcelColumnNum(value = 17)
+    @ExcelProperty(index = 17)
     private String ticketOutsideProcedureFeeOrgName;
     /**
      * 手续费(票外）比例
      */
-    @ExcelColumnNum(value = 18)
+    @ExcelProperty(index = 18)
     private String ticketOutsideProcedureFeeRate;
     /**
      * 手续费（票外）金额
      */
-    @ExcelColumnNum(value = 19)
+    @ExcelProperty(index = 19)
     private BigDecimal ticketOutsideProcedureFeeAmount;
     /**
      * 应付佣金-代理人姓名
      */
-    @ExcelColumnNum(value = 20)
+    @ExcelProperty(index = 20)
     private String agentName;
     /**
      * 应付佣金-代理人身份证号
      */
-    @ExcelColumnNum(value = 21)
+    @ExcelProperty(index = 21)
     private String agentIdNo;
     /**
      * 应付佣金-代理人银行卡号
      */
-    @ExcelColumnNum(value = 22)
+    @ExcelProperty(index = 22)
     private String agentBankCardNo;
     /**
      * 应付佣金-代理人银行预留手机号
      */
-    @ExcelColumnNum(value = 23)
+    @ExcelProperty(index = 23)
     private String agentBankMobile;
     /**
      * 应付佣金-佣金比例
      */
-    @ExcelColumnNum(value = 24)
+    @ExcelProperty(index = 24)
     private String brokerageRate;
     /**
      * 应付佣金-佣金金额
      */
-    @ExcelColumnNum(value = 25)
+    @ExcelProperty(index = 25)
     private BigDecimal brokerageAmount;
 
 }
