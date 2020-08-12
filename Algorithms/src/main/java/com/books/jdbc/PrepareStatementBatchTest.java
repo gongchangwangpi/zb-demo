eff9f52288ae65ee6fe6aafe70897e0702e4bd18
张玻 <zhangbo@mydataway.com>
2020-08-12 11:56:58 +0800

    executeBatch commit

--- Algorithms/src/main/java/com/books/jdbc/PrepareStatementBatchTest.java
+++ Algorithms/src/main/java/com/books/jdbc/PrepareStatementBatchTest.java
@@ -5,6 +5,7 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 /**
  * PreparedStatement 批处理
@@ -15,11 +16,12 @@ import java.util.Arrays;
  */
 public class PrepareStatementBatchTest {
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException, InterruptedException {
 
         String sql = "insert into t_user (username, age, create_time, register_time) values (?, ?, ?, ?);";
 
         Connection connection = JdbcUtil.getConnection();
+        connection.setAutoCommit(false);
         PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
         for (int i = 0; i < 5; i++) {
@@ -28,9 +30,17 @@ public class PrepareStatementBatchTest {
             preparedStatement.setDate(3, new Date(System.currentTimeMillis()));
             preparedStatement.setDate(4, new Date(System.currentTimeMillis()));
             preparedStatement.addBatch();
+            if (i % 3 == 0) {
+                // executeBatch 在设置手动提交后，需要配置commit才能提交数据
+                // 如果是自动提交，则每次 executeBatch 都会提交成功。
+                preparedStatement.executeBatch();
+                TimeUnit.SECONDS.sleep(1);
+            }
         }
 
         int[] result = preparedStatement.executeBatch();
+
+        connection.commit();
 
         System.out.println(Arrays.toString(result));
 
