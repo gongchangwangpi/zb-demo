9df164ef50c4be115fe871c88183c268aefe6d38
张玻 <zhangbo@mydataway.com>
2020-08-20 14:59:06 +0800

    省市区行政区域

--- Algorithms/src/main/java/com/test/hutool/excel/DistrictTest.java
+++ Algorithms/src/main/java/com/test/hutool/excel/DistrictTest.java
@@ -28,11 +28,55 @@ public class DistrictTest {
 //        List<Map<String, Object>> list = excelReader.readAll();
 //        System.out.println(list.size());
 
-        List<Region> originRegionList = EasyExcelFactory.read(inputStream).head(Region.class).doReadAllSync();
+        List<Region> originRegionList = EasyExcelFactory.read(inputStream).sheet(1).head(Region.class).doReadSync();
         List<Region> regionList = new ArrayList<>(originRegionList.size());
 
         System.out.println("共读取到区域：" + originRegionList.size());
 
+        Region one = null;
+        Region two = null;
+
+        for (Region region : originRegionList) {
+            if (region.getName().startsWith("   ")) {
+                region.setLevel(3);
+                region.setParentCode(two.getCode());
+            } else if (region.getName().startsWith(" ")) {
+                two = region;
+                region.setLevel(2);
+                region.setParentCode(one.getCode());
+            } else {
+                one = region;
+                region.setLevel(1);
+            }
+            region.setName(region.getName().replaceAll(" ", ""));
+            regionList.add(region);
+            System.out.println(region);
+        }
+        System.out.println(regionList.size());
+
+//        process(originRegionList, regionList);
+
+//        Map<String, List<Region>> one = originRegionList.stream().collect(groupingBy(region -> {
+//            return region.getCode().substring(0, 2);
+//        }));
+//
+//        List<Map<String, List<Region>>> twoList = new ArrayList<>();
+//        for (List<Region> regions : one.values()) {
+//            Map<String, List<Region>> two = regions.stream().collect(groupingBy(region -> {
+//                return region.getCode().substring(0, 4);
+//            }));
+//            twoList.add(two);
+//        }
+//
+//        System.out.println(twoList.size());
+//
+//        for (Map<String, List<Region>> map : twoList) {
+//
+//        }
+
+    }
+
+    private static void process(List<Region> originRegionList, List<Region> regionList) {
         List<Region> oneLevelList = originRegionList.stream().filter(region -> {
             return region.getCode().endsWith("0000");
         }).collect(Collectors.toList());
@@ -86,25 +130,6 @@ public class DistrictTest {
         }
 
         System.out.println(regionList.size());
-
-//        Map<String, List<Region>> one = originRegionList.stream().collect(groupingBy(region -> {
-//            return region.getCode().substring(0, 2);
-//        }));
-//
-//        List<Map<String, List<Region>>> twoList = new ArrayList<>();
-//        for (List<Region> regions : one.values()) {
-//            Map<String, List<Region>> two = regions.stream().collect(groupingBy(region -> {
-//                return region.getCode().substring(0, 4);
-//            }));
-//            twoList.add(two);
-//        }
-//
-//        System.out.println(twoList.size());
-//
-//        for (Map<String, List<Region>> map : twoList) {
-//
-//        }
-
     }
 
     @Data
@@ -121,7 +146,8 @@ public class DistrictTest {
         }
 
         public void setName(String name) {
-            this.name = StringUtils.trimToEmpty(name).replaceAll(" ", "");
+//            this.name = StringUtils.trimToEmpty(name).replaceAll(" ", "");
+            this.name = name;
         }
 
         @Override
