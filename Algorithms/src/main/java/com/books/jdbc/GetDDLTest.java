1b3a1ce9b81c69562e2f9fc762fe228066f0212b
张玻 <zhangbo@mydataway.com>
2020-09-04 14:48:59 +0800

    jdbc get DDL test

---
+++ Algorithms/src/main/java/com/books/jdbc/GetDDLTest.java
@@ -0,0 +1,29 @@
+package com.books.jdbc;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * @author zhangbo
+ * @date 2020/9/4
+ */
+public class GetDDLTest {
+
+    public static void main(String[] args) throws SQLException {
+        Connection connection = JdbcUtil.getConnection();
+
+        String sql = "show create table t_region";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+            // 表名
+            System.out.println(resultSet.getString(1));
+            // DDL
+            System.out.println(resultSet.getString(2));
+        }
+    }
+
+}
